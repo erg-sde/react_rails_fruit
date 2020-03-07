0f146446426e4fbb537c00fbaf1b693ec9f7d624
Ed <ergonzalezpmp@gmail.com>
2020-03-07 11:54:02 -0500

    added home controller, route, and view, connected view to React

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api do 
-    namespace :v1 do 
-     resources :fruits, only: [:index, :create, :destroy, :update]
-    end 
-  end 
+  namespace :api do
+    namespace :v1 do
+      resources :fruits, only: %i[index create destroy update]
+    end
+  end
+  root to: 'home#index'
 end
